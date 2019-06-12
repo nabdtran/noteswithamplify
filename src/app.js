5fa5341029b7374bc088feeba861e92433c9b5c0
Danny <danny.tran@nab.com.au>
2019-06-12 09:49:28 +0000

    dev

---
+++ src/app.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { withAuthenticator } from 'aws-amplify-react'; // or 'aws-amplify-react-native';
+import Amplify from 'aws-amplify';
+import logo from './logo.svg';
+import './App.css';
+// Get the aws resources configuration parameters
+import awsconfig from './aws-exports'; // if you are using Amplify CLI
+
+Amplify.configure(awsconfig);
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default withAuthenticator(App, { includeGreetings: true });
